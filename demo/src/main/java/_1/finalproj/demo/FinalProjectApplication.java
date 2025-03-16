dffdbb332d5f081e96c65b4ed70915a4b4466630
BD-S78 <bryandong3@gmail.com>
2025-03-16 16:26:26 -0400

    Add files via upload

---
+++ demo/src/main/java/_1/finalproj/demo/FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package _1.finalproj.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectApplication.class, args);
+	}
+
+}
